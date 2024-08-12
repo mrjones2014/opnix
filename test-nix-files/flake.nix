7e39f4a9db57a47cfee3d2ee5319ad8bc4040c6a
Ivan Petkov <ivanppetkov@gmail.com>
2024-08-12 12:56:10 -0700

    Reference module directly and avoid having to update flake.lock

--- test-nix-files/flake.nix
+++ test-nix-files/flake.nix
@@ -1,19 +1,23 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    opnix = {
-      url = "github:mrjones2014/op-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # opnix = {
+    #   url = "/home/ivan/code/op-nix";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
 
-  outputs = { nixpkgs, opnix, ... }:
+  outputs = { nixpkgs, ... }:
     let system = "x86_64-linux";
     in {
       # test is a hostname for our machine
       nixosConfigurations.test = nixpkgs.lib.nixosSystem {
         inherit system;
-        modules = [ opnix.nixosModules.default ./configuration.nix ];
+        modules = [
+          #opnix.nixosModules.default
+          ../modules/op-secrets.nix
+          ./configuration.nix
+        ];
       };
     };
 }
