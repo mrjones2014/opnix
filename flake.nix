7ecb831fd0ec8163fcdace5f231398a741e82131
Mat Jones <mat@mjones.network>
2024-08-12 10:37:08 -0400

    add flake

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = {
+    nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
+    flake-utils = { url = "github:numtide/flake-utils"; };
+  };
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "shell with nixos-shell CLI utility";
+
+          packages = with pkgs; [ nixos-shell ];
+        };
+      });
+}
