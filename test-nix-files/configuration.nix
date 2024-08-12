20f04d1dd3037e3e96b8bf5c160a81330af47410
Ivan Petkov <ivanppetkov@gmail.com>
2024-08-12 15:24:38 -0700

    Increase VM memory to 4GB

--- test-nix-files/configuration.nix
+++ test-nix-files/configuration.nix
@@ -25,7 +25,7 @@
   virtualisation.vmVariant = {
     # following configuration is added only when building VM with build-vm
     virtualisation = {
-      memorySize = 2048; # Use 2048MiB memory.
+      memorySize = 4 * 1024; # Use 4Gb memory.
       cores = 3;
       graphics = false;
     };
