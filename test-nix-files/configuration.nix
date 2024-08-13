f3828c7e609b19bd1cd4e79de82f15b00d82bb7b
Mat Jones <mat@mjones.network>
2024-08-13 11:10:46 -0400

    remove token from source file

--- test-nix-files/configuration.nix
+++ test-nix-files/configuration.nix
@@ -2,12 +2,6 @@
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [ "1password-cli" ];
   imports = [ ./homepage.nix ];
-  environment.etc."op_service_account_token" = {
-    text = ''
-      ops_eyJzaWduSW5BZGRyZXNzIjoibXkuMXBhc3N3b3JkLmNvbSIsInVzZXJBdXRoIjp7Im1ldGhvZCI6IlNSUGctNDA5NiIsImFsZyI6IlBCRVMyZy1IUzI1NiIsIml0ZXJhdGlvbnMiOjY1MDAwMCwic2FsdCI6IlU4TGlJWWJxeTdZZUtVRmRSWUE5blEifSwiZW1haWwiOiJ5bzU1bnFmaGhyMm5rQDFwYXNzd29yZHNlcnZpY2VhY2NvdW50cy5jb20iLCJzcnBYIjoiNmJiN2FlOTg2YzI0YmQyZTc1MGQxYTNkNzk4M2M1NWNjOTFmMzBmYjMzNTYyYmE4NjU4NzdiNjgxOTc3MmQ3YiIsIm11ayI6eyJhbGciOiJBMjU2R0NNIiwiZXh0Ijp0cnVlLCJrIjoiZDNVVE8xUmpvSjJSbTk2eUFZTEpZMkUyUm1aU3BTRHJ4MkRhS3libGdBYyIsImtleV9vcHMiOlsiZW5jcnlwdCIsImRlY3J5cHQiXSwia3R5Ijoib2N0Iiwia2lkIjoibXAifSwic2VjcmV0S2V5IjoiQTMtTERWOEFELUMzWTRLTi1ZM1I0NC1NQ1czVC1aR0haNS1WOFlOTSIsInRocm90dGxlU2VjcmV0Ijp7InNlZWQiOiI1MGJkZjE1MzE4OGU1YjAyM2FkOWViZGI2MTNjNWNmY2UwNDY2NzU1YTFhZTU0Nzk1M2E5ZjNlM2QzYzMwYjE4IiwidXVpZCI6IkVNSFVYTFNTUVJEMkJHNlZMTEo1N05aN1ZVIn0sImRldmljZVV1aWQiOiJ3NG9iZTRxYXQ1dWJqYmNkdHlwZWZla3FobSJ9
-    '';
-    mode = "0400";
-  };
   # customize kernel version
   boot.kernelPackages = pkgs.linuxPackages_5_15;
 
