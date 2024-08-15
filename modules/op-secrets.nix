10fdd15b645bb77f150d56bfd3f5df54e86f3918
Mat Jones <mat@mjones.network>
2024-08-15 12:21:10 -0400

    remove RemainAfterExit

--- modules/op-secrets.nix
+++ modules/op-secrets.nix
@@ -79,8 +79,6 @@ in {
         serviceConfig = {
           Type = "oneshot";
           EnvironmentFile = cfg.environmentFile;
-          RemainAfterExit = true;
-          ExecReload = opnixScript;
         };
 
         script = opnixScript;
